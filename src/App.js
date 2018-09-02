34845489a067bb345ef4e5a2a28da4230a817a7c
Mostafa <mostafahamed73@yahoo.com>
2018-09-02 15:18:05 +0200

    styles stepper

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        
+        <div className='step-progress'>
         <StepZilla steps={steps} />
-    
+        </div>
       </div>
     );
   }
